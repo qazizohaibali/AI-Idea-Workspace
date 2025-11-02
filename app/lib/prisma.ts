3f61618c12bd50892cb41072d3ca27edd370179e
qazizohaibali <zebikazi33@gmail.com>
2025-11-02 16:49:53 +0500

    fix: package.json

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,11 +1,13 @@
-import { PrismaClient } from "../generated/prisma/client";
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+const prisma = global.prisma ?? new PrismaClient();
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
 export default prisma;
